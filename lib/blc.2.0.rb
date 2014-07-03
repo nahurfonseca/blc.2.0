ac04785b3ec1029b6d0e8840ea82ab989803e317
Nahur Fonseca <nahur.fonseca@gmail.com>
2014-07-03 16:05:26 -0400

    Hoe sowed structed

---
+++ lib/blc.2.0.rb
@@ -0,0 +1,3 @@
+class Blc.2.0
+  VERSION = "1.0.0"
+end
---
+++ test/test_blc.2.0.rb
@@ -0,0 +1,9 @@
+gem "minitest"
+require "minitest/autorun"
+require "blc.2.0"
+
+class TestBlc.2.0 < Minitest::Test
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
